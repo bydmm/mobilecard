932bc456ff07265c0beb0d25895b6e329002a293
tuliang <info@tuliang.org>
2013-03-01 19:02:19 +0800

    神奇的分号

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
 define('APP_NAME','FrontEnd');
 define('APP_PATH','./FrontEnd/');
+//开启调试模式
+define('APP_DEBUG', true);
+
+// 采用rest模式运行 
+define('MODE_NAME', 'rest');
+
 require 'ThinkPHP/ThinkPHP.php';
