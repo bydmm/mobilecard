03a160da1e8494e9c5aa9475cee2e40ed4409d01
tuliang <info@tuliang.org>
2013-03-04 19:54:25 +0800

    修正拖拽BUG

--- public/js/html5_drag_and_drop.js
+++ public/js/html5_drag_and_drop.js
@@ -3,60 +3,68 @@
 		var dragSrcEl = null;
 
 		function handleDragStart(e) {
-		  // Target (this) element is the source node.
-		  //this.style.opacity = '0.4';
-		  this.classList.add('drag');
+			if ($(this).hasClass('custombtn')) return false;
 
-		  dragSrcEl = this;
+			// Target (this) element is the source node.
+			this.classList.add('drag');
 
-		  e.dataTransfer.effectAllowed = 'move';
-		  e.dataTransfer.setData('text/html', this.innerHTML);
+			dragSrcEl = this;
 		}
 
 		function handleDragEnter(e) {
+			if ($(this).hasClass('custombtn')) return false;
+			if (!dragSrcEl) return false;
+
 			// this / e.target is the current hover target.
 			this.classList.add('over');
 		}
 
 		function handleDragOver(e) {
-		  if (e.preventDefault) {
-		    e.preventDefault(); // Necessary. Allows us to drop.
-		  }
+			if ($(this).hasClass('custombtn')) return false;
+			if (!dragSrcEl) return false;
 
-		  e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+			if (e.preventDefault) {
+				e.preventDefault(); // Necessary. Allows us to drop.
+			}
 
-		  return false;
+			return false;
 		}
 
 		function handleDragLeave(e) {
-			//this.style.opacity = '1.0';
+			if ($(this).hasClass('custombtn')) return false;
+			if (!dragSrcEl) return false;
+
 			this.classList.remove('drag');
-		  this.classList.remove('over');  // this / e.target is previous target element.
+			this.classList.remove('over');  // this / e.target is previous target element.
 		}
 
 		function handleDrop(e) {
-		  // this/e.target is current target element.
+			if ($(this).hasClass('custombtn')) return false;
+			if (!dragSrcEl) return false;
 
-		  if (e.stopPropagation) {
-		    e.stopPropagation(); // Stops some browsers from redirecting.
-		  }
+			// this/e.target is current target element.
 
-		  // Don't do anything if dropping the same column we're dragging.
-		  if (dragSrcEl != this) {
-		    // Set the source column's HTML to the HTML of the column we dropped on.
-		    dragSrcEl.innerHTML = this.innerHTML;
-		    this.innerHTML = e.dataTransfer.getData('text/html');
-		  }
+			// Don't do anything if dropping the same column we're dragging.
+			if (dragSrcEl != this) {
 
-		  return false;
+				// Set the source column's HTML to the HTML of the column we dropped on.
+				dragSrcEl.innerHTML = this.innerHTML;
+				this.innerHTML = e.dataTransfer.getData('text/html');
+			}
+
+			return false;
 		}
 
 		function handleDragEnd(e) {
-		  // this/e.target is the source node.
+			if ($(this).hasClass('custombtn')) return false;
+			if (!dragSrcEl) return false;
+			
+			// this/e.target is the source node.
 
-		  [].forEach.call(cols, function (col) {
-		    col.classList.remove('over');
-		  });
+			[].forEach.call(cols, function (col) {
+				col.classList.remove('drag');
+				col.classList.remove('over');
+			});
 		}
 
 		var cols = document.querySelectorAll('.basic .custombtn');
