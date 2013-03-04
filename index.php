fc4654638c3964642d6194c78c55e220b71fd123
bydmm <521194743@qq.com>
2013-03-04 17:28:44 +0800

    index.php

--- index.php
+++ index.php
@@ -7,14 +7,10 @@ define('APP_DEBUG', true);
 // 采用rest模式运行 
 define('MODE_NAME', 'rest');
 
-<<<<<<< HEAD
 //url常量
 define('PUBLIC', 'Public/');
 define('IMAGES', 'Public/images');
 define('CSS', 'Public/css');
 define('JS', 'Public/js');
 
-
-=======
->>>>>>> da0c8383b35006db7f8361aea054070cb50675b8
 require 'ThinkPHP/ThinkPHP.php';
