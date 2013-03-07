a039c712a596f4b2061fd962c693cb8c15dd496c
bydmm <521194743@qq.com>
2013-03-07 19:08:18 +0800

    title and logo

---
+++ public/js/editor.js
@@ -0,0 +1,27 @@
+KindEditor.ready(function(K) {
+	var editor = K.editor({
+		allowFileManager : true,
+		langType : 'en'
+	});
+	K('#drag-images').click(function() {
+		editor.loadPlugin('image', function() {
+			editor.plugin.imageDialog({
+				showRemote : false,
+				imageUrl : K('#url3').val(),
+				clickFn : function(url, title, width, height, border, align) {
+					K('#url3').val(url);
+					K('img.logo').attr('src',url);
+					editor.hideDialog();
+				}
+			});
+		});
+		return false;
+	});
+	
+	//pagetitle
+	$("#pagetitle").val($('.preview .pagetitle').html());
+	$("#pagetitle").keyup(function(){
+		var title = $(this).val();
+		$('.preview .pagetitle').html(title);
+	});
+});
