8793369a5e3c4dc42d50cc5afcdf822400486217
tuliang <info@tuliang.org>
2013-03-09 14:48:58 +0800

    更新数据库参数

--- FrontEnd/Conf/config.php
+++ FrontEnd/Conf/config.php
@@ -5,9 +5,9 @@ return array(
     'DB_TYPE'   => 'mysql', // 数据库类型
     'DB_HOST'   => '192.168.1.88', // 服务器地址
     'DB_NAME'   => 'visitingcard', // 数据库名
-    'DB_USER'   => 'root', // 用户名
-    'DB_PWD'    => '', // 密码
+    'DB_USER'   => 'admin', // 用户名
+    'DB_PWD'    => 'admin', // 密码
     'DB_PORT'   => 3306, // 端口
-    'DB_PREFIX' => 'think_', // 数据库表前缀 
+    'DB_PREFIX' => 'pmc_', // 数据库表前缀 
 );
 ?>
